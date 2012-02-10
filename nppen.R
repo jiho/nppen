4c20cfa0bc5b02948a71d04b70d6a319a7f7d095
Jean-Olivier Irisson <irisson@normalesup.org>
2012-02-10 13:57:38 +0100

    New implementation
    - matrix based implementation (instead of using mahalanobis() on data.frames)
    - fast approximate implementation based on a suggestion by F Ibanez
    - new tests and better grahics

--- nppen.R
+++ nppen.R
@@ -43,33 +43,103 @@ rasterize <- function(X, n=10, precisions=sapply(lapply(X,range),diff)/n)
    return(X)
 }
 
-nppen <- function(X, Y)
+nppen <- function(X, Y, fast=TRUE)
 #
-#   Compute the probability of presence of a given taxon based on
-#   the environmental descriptors
+#   Compute the probability of presence of a given taxon based on the
+#   environmental descriptors
 #
-#   X	matrix or data.frame containing the environmental data at
-#   	locations of presence (possibly rasterized)
+#   X	matrix or data.frame containing the environmental data at locations of
+#       presence (possibly rasterized)
 #
-#   Y	matrix or data.frame containing the environmental data at
-#   	the points where the probability of presence needs to be
-#   	predicted (i.e., usually on a grid)
+#   Y	matrix or data.frame containing the environmental data at the points
+#       where the probability of presence needs to be predicted (i.e., usually
+#       on a grid)
+#
+#   fast    when TRUE, a single total covariance matrix is used for each
+#           element of Y, instead of one matrix per permutation (i.e. per line
+#           of X). This is immensely faster and leads to only marginal (<2%)
+#           changes in the predicted probabilities as long as X is large and
+#           points of Y as distributed within the range covered by X
 #
 {
+    # convert to matrices, for speed
+    X = as.matrix(X)
+    Y = as.matrix(Y)
 
-	# vector sizes
+	# sizes
 	n = nrow(X)
 	m = nrow(Y)
 
 	# compute reference distance for each point of Y:
-	# the actual Mahalanobis distance to X
+	#   the actual Mahalanobis distance to X
+    # NB: mahalanobis() has
+    #   sweep(Y, 2, colMeans(X))
+    # but this
+	Yc = t(t(Y)-colMeans(X))
+	# is faster
+	invCov = solve(cov(X))
+	d2 = rowSums((Yc %*% invCov) * Yc)
+
+	# compute the probabilities of occurrence of those distances:
+	#   for a given element of Y (y_j), swap it with an element of X (x_i)
+	#   and compute the distance from x_i to X - x_i + y_j
+	#   then count how many of these permutations lead to a distance
+	#   larger than distance from y_j to X
+	p = rep(-1, m)  # NB: pre-allocating the vector is a bit faster
+	if (fast) {
+
+    	for (iy in 1:m) {
+    		y = Y[iy,]
+
+            # use a global matrix which is y + X instead of swapping y for
+            # each element of X
+    		Z = rbind(y, X)
+    		inCov = solve(cov(Z))
+    		Xc = t(t(X) - colMeans(Z))
+    		d2m = rowSums((Xc %*% invCov) * Xc)
+
+            # compute the probability
+    		p[iy] = sum(d2m > d2[iy]) / n
+    	}
+
+	} else {
+
+    	for (iy in 1:m) {
+    		y = Y[iy,]
+
+    		d2m = rep(-1, n)
+    		for (ix in 1:n) {
+    		    # create the matrix X - x_i + y_j
+    			Z = rbind(y, X[-ix,])
+
+                # compute mahalanobis' distance
+    			Xc = X[ix,] - colMeans(Z)
+            	invCov = solve(cov(Z))
+                d2m[ix] = c(Xc %*% invCov %*% Xc)
+                # NB: since X[ix,] is a vector, we don't have to worry about the transpositions and the rowSums
+    		}
+
+            # compute the probability
+    		p[iy] = sum(d2m > d2[iy]) / n
+    	}
+
+	}
+
+	return(p)
+}
+
+# Reference implementation (slow but accurate)
+nppen.ref <- function(X, Y) {
+    X = as.matrix(X)
+    Y = as.matrix(Y)
+
+	n = nrow(X)
+	m = nrow(Y)
+
+	# compute reference distance for each point of Y
 	d2 = mahalanobis(Y, colMeans(X), cov(X))
 
 	# compute the probabilities of occurrence of those distances
-	# for each element of Y (y_j), swap it with an element of X (x_i)
-	# and compute the distance from x_i to X - x_i + y_j
-	# then count how many of these permutations lead to a distance
-	# larger than the reference one
 	p = c()
 	for (iy in 1:m) {
 		y = Y[iy,]
