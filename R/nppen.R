9155e2972657b52c0972800258d08628814466e7
Jean-Olivier Irisson <irisson@normalesup.org>
2017-01-03 14:37:23 +0100

    Change tabs from 4 spaces to 2

--- R/nppen.R
+++ R/nppen.R
@@ -45,97 +45,97 @@
 #' nppen(X_binned_common[,-ncol(X_binned)], Y, fast=FALSE)
 #' # reducing to common observations helps better represent the initial niche
 nppen <- function(X, Y, fast=TRUE) {
-    # convert to matrices, for speed
-    X <- as.matrix(X)
-    Y <- as.matrix(Y)
+  # convert to matrices, for speed
+  X <- as.matrix(X)
+  Y <- as.matrix(Y)
 
-    # sizes
-    n <- nrow(X)
-    m <- nrow(Y)
+  # sizes
+  n <- nrow(X)
+  m <- nrow(Y)
 
-    # compute reference distance for each point of Y:
-    #   the actual Mahalanobis distance to X
-    # NB: mahalanobis() has
-    #   sweep(Y, 2, colMeans(X))
-    # but this
-    Yc <- t(t(Y)-colMeans(X))
-    # is faster
-    invCov <- solve(stats::cov(X))
-    d2 <- rowSums((Yc %*% invCov) * Yc)
+  # compute reference distance for each point of Y:
+  #   the actual Mahalanobis distance to X
+  # NB: mahalanobis() has
+  #   sweep(Y, 2, colMeans(X))
+  # but this
+  Yc <- t(t(Y)-colMeans(X))
+  # is faster
+  invCov <- solve(stats::cov(X))
+  d2 <- rowSums((Yc %*% invCov) * Yc)
 
-    # compute the probabilities of occurrence of those distances:
-    #   for a given element of Y (y_j), swap it with an element of X (x_i)
-    #   and compute the distance from x_i to X - x_i + y_j
-    #   then count how many of these permutations lead to a distance
-    #   larger than distance from y_j to X
-    p <- rep(-1, m)  # NB: pre-allocating the vector is a bit faster
-    if (fast) {
+  # compute the probabilities of occurrence of those distances:
+  #   for a given element of Y (y_j), swap it with an element of X (x_i)
+  #   and compute the distance from x_i to X - x_i + y_j
+  #   then count how many of these permutations lead to a distance
+  #   larger than distance from y_j to X
+  p <- rep(-1, m)  # NB: pre-allocating the vector is a bit faster
+  if (fast) {
 
-        for (iy in 1:m) {
-            y <- Y[iy,]
+    for (iy in 1:m) {
+      y <- Y[iy,]
 
-            # use a global matrix which is y + X instead of swapping y for
-            # each element of X
-            Z <- rbind(y, X)
-            inCov <- solve(stats::cov(Z))
-            Xc <- t(t(X) - colMeans(Z))
-            d2m <- rowSums((Xc %*% invCov) * Xc)
+      # use a global matrix which is y + X instead of swapping y for
+      # each element of X
+      Z <- rbind(y, X)
+      inCov <- solve(stats::cov(Z))
+      Xc <- t(t(X) - colMeans(Z))
+      d2m <- rowSums((Xc %*% invCov) * Xc)
 
-            # compute the probability
-            p[iy] <- sum(d2m > d2[iy]) / n
-            # NB: it is actually faster to do this in the loop than to store
-            #     all values in a matrix and compute the probabilities on the
-            #     matrix. I don't understand why, but...
-        }
-
-    } else {
-
-        for (iy in 1:m) {
-            y <- Y[iy,]
-
-            d2m <- rep(-1, n)
-            for (ix in 1:n) {
-                # create the matrix X - x_i + y_j
-                Z <- rbind(y, X[-ix,])
-
-                # compute mahalanobis' distance
-                Xc <- X[ix,] - colMeans(Z)
-                invCov <- solve(stats::cov(Z))
-                d2m[ix] <- c(Xc %*% invCov %*% Xc)
-                # NB: since X[ix,] is a vector, we don't have to worry about the transpositions and the rowSums
-            }
-
-            # compute the probability
-            p[iy] <- sum(d2m > d2[iy]) / n
-        }
-
+      # compute the probability
+      p[iy] <- sum(d2m > d2[iy]) / n
+      # NB: it is actually faster to do this in the loop than to store
+      #     all values in a matrix and compute the probabilities on the
+      #     matrix. I don't understand why, but...
     }
 
-    return(p)
+  } else {
+
+    for (iy in 1:m) {
+      y <- Y[iy,]
+
+      d2m <- rep(-1, n)
+      for (ix in 1:n) {
+        # create the matrix X - x_i + y_j
+        Z <- rbind(y, X[-ix,])
+
+        # compute mahalanobis' distance
+        Xc <- X[ix,] - colMeans(Z)
+        invCov <- solve(stats::cov(Z))
+        d2m[ix] <- c(Xc %*% invCov %*% Xc)
+        # NB: since X[ix,] is a vector, we don't have to worry about the transpositions and the rowSums
+      }
+
+      # compute the probability
+      p[iy] <- sum(d2m > d2[iy]) / n
+    }
+
+  }
+
+  return(p)
 }
 
 # # Reference implementation (slow but accurate)
 # nppen_ref <- function(X, Y) {
-#     X <- as.matrix(X)
-#     Y <- as.matrix(Y)
+#   X <- as.matrix(X)
+#   Y <- as.matrix(Y)
 #
-#     n <- nrow(X)
-#     m <- nrow(Y)
+#   n <- nrow(X)
+#   m <- nrow(Y)
 #
-#     # compute reference distance for each point of Y
-#     d2 <- stats::mahalanobis(Y, colMeans(X), stats::cov(X))
+#   # compute reference distance for each point of Y
+#   d2 <- stats::mahalanobis(Y, colMeans(X), stats::cov(X))
 #
-#     # compute the probabilities of occurrence of those distances
-#     p <- c()
-#     for (iy in 1:m) {
-#         y <- Y[iy,]
-#         d2m <- c()
-#         for (ix in 1:n) {
-#             Z <- rbind(y, X[-ix,])
-#             d2m[ix] <- stats::mahalanobis(X[ix,], colMeans(Z), stats::cov(Z))
-#         }
-#         p[iy] <- sum(d2m > d2[iy]) / n
+#   # compute the probabilities of occurrence of those distances
+#   p <- c()
+#   for (iy in 1:m) {
+#     y <- Y[iy,]
+#     d2m <- c()
+#     for (ix in 1:n) {
+#       Z <- rbind(y, X[-ix,])
+#       d2m[ix] <- stats::mahalanobis(X[ix,], colMeans(Z), stats::cov(Z))
 #     }
+#     p[iy] <- sum(d2m > d2[iy]) / n
+#   }
 #
-#     return(p)
+#   return(p)
 # }
