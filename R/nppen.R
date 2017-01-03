f79fc751a3b06ec26d76f2ff47564a71575b9d66
Jean-Olivier Irisson <irisson@normalesup.org>
2017-01-03 14:34:25 +0100

    Add the actual NPPEN function
    Improve documentation
    Add examples
    explicitly refer to the stats:: functions

--- R/nppen.R
+++ R/nppen.R
@@ -1,35 +1,50 @@
+#' Non-Parametric Probabilistic Ecological Niche
+#'
+#' Compute the probability of presence of a taxon based on the environment.
 #
-#     Non-Parametric Probabilistic Ecological Niche
-#
-#     Beaugrand, G, Lenoir, S, Ibañez, F, and Manté, C
-#     A new model to assess the probability of occurrence of a species,
-#     based on presence-only data
-#     Marine Ecology Progress Series, 424:175--190, 2011
-#
-# (c) Copyright 2011 Jean-Olivier Irisson
-#     GNU General Public License v3
-#
-#------------------------------------------------------------
-
-nppen <- function(X, Y, fast=TRUE)
-#
-#   Compute the probability of presence of a given taxon based on the
-#   environmental descriptors
-#
-#   X   matrix or data.frame containing the environmental data at locations of
-#       presence (possibly rasterized)
-#
-#   Y   matrix or data.frame containing the environmental data at the points
-#       where the probability of presence needs to be predicted (i.e., usually
-#       on a grid)
-#
-#   fast    when TRUE, a single total covariance matrix is used for each
-#           element of Y, instead of one matrix per permutation (i.e. per line
-#           of X). This is immensely faster and leads to only marginal (<2%)
-#           changes in the predicted probabilities as long as X is large and
-#           points of Y as distributed within the range covered by X
-#
-{
+#' @param X matrix or data.frame containing the environmental data at locations of presence (possibly rasterized using \code{\link{rasterize}}).
+#' @param Y matrix or data.frame containing the environmental data at the points where the probability of presence needs to be predicted (i.e., usually on a grid).
+#' @param fast when TRUE, a single total covariance matrix is used for each element of Y, instead of one matrix per permutation (i.e. per line of X). This is immensely faster and leads to only marginal (<2\%) changes in the predicted probabilities as long as X is large and points of Y are distributed within the range covered by X (which they should be for the model to make sense anyway).
+#'
+#' @return A vector of probabilities of length \code{nrow(Y)}.
+#'
+#' @references
+#' Beaugrand, G., Lenoir, S., Ibañez, F., and Manté, C. (2011) _A new model to assess the probability of occurrence of a species, based on presence-only data_. Marine Ecology Progress Series, *424*, 175-190. \url{http://www.int-res.com/abstracts/meps/v424/p175-190/}
+#' @export
+#'
+#' @examples
+#' # define environmental data
+#' set.seed(1)
+#' X <- data.frame(temp=rnorm(200, 15, 3), sal=rnorm(200, 37, 1))
+#' # define target points: one well inside the niche, one on the border
+#' Y <- data.frame(temp=c(15, 18.7), sal=c(37, 38.7))
+#' # represent both in environmental space
+#' plot(X$temp, X$sal)
+#' points(Y$temp, Y$sal, col="red")
+#' # compute probability of presence
+#' nppen(X, Y, fast=FALSE)
+#' nppen(X, Y, fast=TRUE)
+#' # higher probability for the first point, as expected
+#'
+#' # rasterize X to make is smaller (hence faster to compute) and avoid spatial
+#' # bias in the underlying data (e.g. preferential sampling in a give area)
+#' X_binned <- rasterize(X)
+#' nrow(X)
+#' nrow(X_binned)
+#' plot(X_binned$temp, X_binned$sal)
+#' points(Y$temp, Y$sal, col="red")
+#' # compute probability of presence
+#' nppen(X_binned[,-ncol(X_binned)], Y, fast=FALSE) # NB: remove column `n`
+#' # simply binning loose some information regarding where the centre of the
+#' # niche is and the niche appear more spread (hence higher probability for
+#' # the second point here).
+#'
+#' # only keep common observations (= observed more than once)
+#' X_binned_common <- X_binned[X_binned$n > 1,]
+#' points(X_binned_common$temp, X_binned_common$sal, pch=16)
+#' nppen(X_binned_common[,-ncol(X_binned)], Y, fast=FALSE)
+#' # reducing to common observations helps better represent the initial niche
+nppen <- function(X, Y, fast=TRUE) {
     # convert to matrices, for speed
     X <- as.matrix(X)
     Y <- as.matrix(Y)
@@ -45,7 +60,7 @@ nppen <- function(X, Y, fast=TRUE)
     # but this
     Yc <- t(t(Y)-colMeans(X))
     # is faster
-    invCov <- solve(cov(X))
+    invCov <- solve(stats::cov(X))
     d2 <- rowSums((Yc %*% invCov) * Yc)
 
     # compute the probabilities of occurrence of those distances:
@@ -62,7 +77,7 @@ nppen <- function(X, Y, fast=TRUE)
             # use a global matrix which is y + X instead of swapping y for
             # each element of X
             Z <- rbind(y, X)
-            inCov <- solve(cov(Z))
+            inCov <- solve(stats::cov(Z))
             Xc <- t(t(X) - colMeans(Z))
             d2m <- rowSums((Xc %*% invCov) * Xc)
 
@@ -85,7 +100,7 @@ nppen <- function(X, Y, fast=TRUE)
 
                 # compute mahalanobis' distance
                 Xc <- X[ix,] - colMeans(Z)
-                invCov <- solve(cov(Z))
+                invCov <- solve(stats::cov(Z))
                 d2m[ix] <- c(Xc %*% invCov %*% Xc)
                 # NB: since X[ix,] is a vector, we don't have to worry about the transpositions and the rowSums
             }
@@ -100,7 +115,7 @@ nppen <- function(X, Y, fast=TRUE)
 }
 
 # Reference implementation (slow but accurate)
-nppen.ref <- function(X, Y) {
+nppen_ref <- function(X, Y) {
     X <- as.matrix(X)
     Y <- as.matrix(Y)
 
@@ -108,7 +123,7 @@ nppen.ref <- function(X, Y) {
     m <- nrow(Y)
 
     # compute reference distance for each point of Y
-    d2 <- mahalanobis(Y, colMeans(X), cov(X))
+    d2 <- stats::mahalanobis(Y, colMeans(X), stats::cov(X))
 
     # compute the probabilities of occurrence of those distances
     p <- c()
@@ -117,7 +132,7 @@ nppen.ref <- function(X, Y) {
         d2m <- c()
         for (ix in 1:n) {
             Z <- rbind(y, X[-ix,])
-            d2m[ix] <- mahalanobis(X[ix,], colMeans(Z), cov(Z))
+            d2m[ix] <- stats::mahalanobis(X[ix,], colMeans(Z), stats::cov(Z))
         }
         p[iy] <- sum(d2m > d2[iy]) / n
     }
