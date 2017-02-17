ad2ab22b3d8d07a82b0475a4766134766085fd1e
Jean-Olivier Irisson <irisson@normalesup.org>
2017-02-17 01:51:18 +0100

    Better data notation

--- scratch/rasterisation_test.R
+++ scratch/rasterisation_test.R
@@ -9,15 +9,15 @@ library("tidyverse")
 ## Simulate data
 set.seed(123)
 
-n <- 5000
+n <- 3000
 
 # simple reference matrix
 # = subset of the possibles
-Xs <- data.frame(V1=rnorm(n), V2=rnorm(n))
+Xr <- data.frame(V1=rnorm(n), V2=rnorm(n))
 
 # biased reference matrix
 # = subset of the possibles + oversampling in some regions of the state space
-Xb <- rbind(Xs, filter(data.frame(V1=rnorm(n), V2=rnorm(n)), V1>0, V2<2.5, V2>-2.5))
+Xb <- rbind(Xr, filter(data.frame(V1=rnorm(n), V2=rnorm(n)), V1>0, V2<2.5, V2>-2.5))
 n <- 1000
 
 # test matrix
@@ -26,7 +26,7 @@ Y <- expand.grid(V1=grid, V2=grid)
 
 # put all data in the same data.frame
 p <- ggplot() + geom_point(aes(V1, V2), alpha=0.5) + coord_fixed()
-p %+% Xs
+p %+% Xr
 p %+% Xb
 p %+% Y
 
@@ -34,23 +34,33 @@ devtools::load_all()
 
 ## Rasterise ----
 
-Xsr <- rasterise(X, n=30)
-p %+% Xs
-p %+% Xsr
+XrR <- rasterise(Xr, n=30)
+p %+% Xr
+p %+% XrR
 
-Xbr <- rasterise(Xb, n=30)
+XbR <- rasterise(Xb, n=30)
 p %+% Xb
-p %+% Xbr
+p %+% XbR
 
 
 ## Test effect of rasterisation on proba ----
 
-Y$ps  <- nppen(Xs, Y[,1:2])
-Y$psr <- nppen(Xsr[,1:2], Y[,1:2])
-Y$pb  <- nppen(Xb, Y[,1:2])
-Y$pbr <- nppen(Xbr[,1:2], Y[,1:2])
+Y$r  <- nppen(Xr, Y[,1:2])
+Y$rR <- nppen(XrR[,1:2], Y[,1:2])
+Y$b  <- nppen(Xb, Y[,1:2])
+Y$bR <- nppen(XbR[,1:2], Y[,1:2])
 
-Yt <- gather(Y, "source", "proba", starts_with("p"))
+Yt <- gather(Y, "source", "proba", -starts_with("V"))
+
+Xr$source  <- "r"
+XrR$source <- "rR"
+Xb$source  <- "b"
+XbR$source <- "bR"
+Xt <- bind_rows(Xr, XrR, Xb, XbR)
+
+ggplot(Xt) + geom_point(aes(V1, V2), alpha=0.1) + geom_vline(aes(xintercept=0), colour="black") + facet_wrap(~source) + coord_fixed()
+ggsave("raster_points.png")
 
 ggplot(Yt) + geom_raster(aes(V1, V2, fill=proba)) + geom_contour(aes(V1, V2, z=proba), colour="black") + geom_vline(aes(xintercept=0), colour="black") + facet_wrap(~source) + scale_fill_distiller(palette="Blues") + coord_fixed() + scale_x_continuous(expand=c(0,0)) + scale_y_continuous(expand=c(0,0))
+ggsave("raster_proba.png")
 # -> not perfect but helps a lot
